2691d0fb7af21f0268c6deebd9647007fe95f984
Noorsimar <singh.noorsimar@gmail.com>
2015-01-18 17:05:49 +0530

    first shoe conv IOS8

--- ShoeConverter/ViewController.swift
+++ ShoeConverter/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mensShoeSizeTextField: UITextField!
+    @IBOutlet weak var mensConvertedShoeSizeLabel: UILabel!
+    @IBOutlet weak var womensShoeSizeTextField: UITextField!
+    @IBOutlet weak var womensConvertedShoeSizeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +24,34 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func convertButtonPressed(sender: UIButton) {
+//        let sizeFromTextField = mensShoeSizeTextField.text
+//        let numberFromTextField = sizeFromTextField.toInt()
+//        var integerFromTextField = numberFromTextField!
+        let integerFromTextField = mensShoeSizeTextField.text.toInt()!
+        
+        let conversionsConstant = 30
+       // integerFromTextField += conversionsConstant
+        
+        mensConvertedShoeSizeLabel.hidden = false
+        //mensConvertedShoeSizeLabel
+        mensConvertedShoeSizeLabel.text = "\(integerFromTextField + conversionsConstant)" + " In European Shoe Size"
+        mensConvertedShoeSizeLabel.textColor = UIColor.blueColor()
+        
+    }
 
+    @IBAction func convertButtonPressedWomen(sender: UIButton) {
+        let sizeFromTextField = Double((womensShoeSizeTextField.text as NSString).doubleValue)
+        
+        let conversionsConstant = 30.5
+        
+        womensConvertedShoeSizeLabel.hidden = false
+
+        womensConvertedShoeSizeLabel.text = "\(sizeFromTextField + conversionsConstant)" + " In European Shoe Size"
+        womensConvertedShoeSizeLabel.textColor = UIColor.redColor()
+    }
 
 }
 
